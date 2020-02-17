4164a72407f38a9f65d8a19fa78b1ce416ed5770
gethshap <zhchuang@ucdavis.edu>
2020-02-17 12:35:16 -0800

    v0.8 few update

--- tps.c
+++ tps.c
@@ -24,8 +24,7 @@ typedef struct count_storage_class {
 
 typedef struct tps_page {
   pthread_t tid;
-  bool is_clone;
-  void *storage;
+  count_storage_class *count_storage;
 } tps_page;
 
 static int find_page(void *data, void *arg) {
@@ -44,54 +43,98 @@ int tps_init(int segv) {
 
 int tps_create(void) {
   /* TODO: Phase 2 */
-
-  /* TODO: find replicate*/
+  enter_critical_section();
   tps_page *target = NULL;
   queue_iterate(tps_queue, find_page, pthread_self(), (void **) &target);
   if (target) {
+    //find the replicate
     return -1;
   }
   tps_page *page = malloc(sizeof(tps_page));
   page->tid = pthread_self();
-  page->is_clone = false;
-  page->storage = mmap(NULL, TPS_SIZE, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, 1, 0);
+  page->count_storage = malloc(sizeof(count_storage_class));
+  page->count_storage->storage = mmap(NULL, TPS_SIZE, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, 1, 0);
+  page->count_storage->count = 1;
   queue_enqueue(tps_queue, (void *) page);
+  exit_critical_section();
   return 0;
 }
 
 int tps_destroy(void) {
   /* TODO: Phase 2 */
+  enter_critical_section();
   tps_page *target = NULL;
   queue_iterate(tps_queue, find_page, pthread_self(), (void **) &target);
   if (!target) {
     return -1;
   }
-  if (target->is_clone == false)
-    munmap(target->count_storage.storage, TPS_SIZE);
+  if (target->count_storage->count == 1) {
+    munmap(target->count_storage->storage, TPS_SIZE);
+    free(target->count_storage);
+  }
   queue_delete(tps_queue, pthread_self());
-
+  exit_critical_section();
   return 0;
 }
 
 int tps_read(size_t offset, size_t length, void *buffer) {
   /* TODO: Phase 2 */
+  enter_critical_section();
   tps_page *target = NULL;
   queue_iterate(tps_queue, find_page, pthread_self(), (void **) &target);
-
   if (offset + length > TPS_SIZE || target == NULL || buffer == NULL) {
     return -1;
   }
-  memcpy(buffer, target->storage + offset, length);
+  memcpy(buffer, target->count_storage->storage + offset, length);
+  exit_critical_section();
   return 0;
 }
 
-int tps_write(size_t offset, size_t length, void *buffer)
-{
-	/* TODO: Phase 2 */
+int tps_write(size_t offset, size_t length, void *buffer) {
+  enter_critical_section();
+  tps_page *target = NULL;
+  queue_iterate(tps_queue, find_page, pthread_self(), (void **) &target);
+  if (offset + length > TPS_SIZE || target == NULL || buffer == NULL) {
+    return -1;
+  }
+
+  if (target->count_storage->count > 1) {
+    count_storage_class *new_count_storage = malloc(sizeof(count_storage_class));
+    new_count_storage->storage = mmap(NULL, TPS_SIZE, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, 1, 0);
+    new_count_storage->count = 1;
+
+    memcpy(new_count_storage->storage, target->count_storage->storage, TPS_SIZE);
+
+    target->count_storage->count--;
+    target->count_storage = new_count_storage;
+  }
+  memcpy(target->count_storage->storage + offset, buffer, length);
+  exit_critical_section();
+  return 0;
 }
 
-int tps_clone(pthread_t tid)
-{
-	/* TODO: Phase 2 */
+int tps_clone(pthread_t tid) {
+  /* TODO: Phase 2 */
+  enter_critical_section();
+  tps_page *self = NULL;
+  queue_iterate(tps_queue, find_page, pthread_self(), (void **) &self);
+
+  tps_page *target = NULL;
+  queue_iterate(tps_queue, find_page, tid, (void **) &target);
+
+  if (self) {
+    return -1;
+  }
+  if (!target) {
+    return -1;
+  }
+  tps_page *page = malloc(sizeof(tps_page));
+  page->tid = pthread_self();
+  page->count_storage = malloc(sizeof(count_storage_class));
+  page->count_storage = target->count_storage;
+  page->count_storage->count++;
+  queue_enqueue(tps_queue, (void *) page);
+  exit_critical_section();
+  return 0;
 }
 

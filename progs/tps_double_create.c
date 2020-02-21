2107843c3a34074300b62d982edeafa3cceb078a
FrancisTX <50384136+FrancisTX@users.noreply.github.com>
2020-02-20 20:44:45 -0800

    Add files via upload
    Final version

---
+++ progs/tps_double_create.c
@@ -0,0 +1,39 @@
+#include <assert.h>
+#include <limits.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <tps.h>
+#include <sem.h>
+
+#define TEST_ASSERT(assert)            \
+do {                        \
+    printf("ASSERT: " #assert " ... ");    \
+    if (assert) {                \
+        printf("PASS\n");        \
+    } else    {                \
+        printf("FAIL\n");        \
+        exit(1);            \
+    }                    \
+} while(0)
+                         
+void *test_write_and_read_struct(){
+    tps_create();
+    TEST_ASSERT(tps_create() == -1);  
+    tps_destroy();
+    return NULL;
+}
+
+
+int main() {
+    pthread_t tid;
+    tps_init(0);
+
+    /*go the double destroy*/
+    pthread_create(&tid, NULL, (void*)test_write_and_read_struct, NULL);
+    pthread_join(tid, NULL);
+
+  return 0;
+}
